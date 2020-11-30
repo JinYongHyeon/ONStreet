65cd34cf05022677dfa50b84d5a546296b98eebd
JEONGSEHUI <tpgml3285@naver.com>
2020-11-30 20:40:19 +0900

    sql 커밋

--- onstreet/src/main/resources/OnStreet.sql
+++ onstreet/src/main/resources/OnStreet.sql
@@ -83,6 +83,9 @@ CREATE TABLE os_follow(
 	CONSTRAINT os_follow_pk PRIMARY KEY(following_id,id),
 	CONSTRAINT os_follow_fk FOREIGN KEY(id) REFERENCES os_member(id) ON DELETE CASCADE
 );
+select * from os_follow;
+
+
 
 --공지사항
 CREATE TABLE os_notice(
@@ -133,6 +136,8 @@ INSERT INTO os_artist(id,sns,account,artist_info) VALUES('biton','instagram','22
 INSERT INTO os_auth(auth_name,id) VALUES('ROLE_MEMBER','bityong');
 INSERT INTO os_auth(auth_name,id) VALUES('ROLE_MEMBER','biton');
 INSERT INTO os_auth(auth_name,id) VALUES('ROLE_ARTIST','biton');
+
+
 
 --로그인
 SELECT m.id,m.password,m.nickname,m.address,m.phone,m.email,m.name,m.age,m.remove_user_date,m.profile,
